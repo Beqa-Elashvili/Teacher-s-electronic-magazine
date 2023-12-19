d2a8cf5e8adff055749d6ec868ca127b38651270
Beqael <beqaelashvili3@gmail.com>
2023-12-19 19:18:57 +0400

    add score-size and enter function

--- src/Features/AddPoints/exitingPointvalue/Add.jsx
+++ src/Features/AddPoints/exitingPointvalue/Add.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useContext } from "react";
 import { SAdd } from "./Addstyle";
-
 import { GlobalContext } from "../../../GlobalContext/GlobalContext";
 
 export function Add({ day, userid }) {
@@ -41,6 +40,21 @@ export function Add({ day, userid }) {
     setMode("view");
   };
 
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      handleUpdate();
+    }
+  };
+
+  const InputChangeValue = (e) => {
+    let newValue = e.target.value;
+    if (newValue !== "" && (isNaN(newValue) || newValue < 1 || newValue > 10)) {
+      newValue = "";
+      alert("Write the score from 1 to 10");
+    }
+    setPoint(newValue);
+  };
+
   return (
     <SAdd>
       {mode === "view" && (
@@ -51,15 +65,13 @@ export function Add({ day, userid }) {
       {mode === "update" && (
         <>
           <input
-          type="number"
+            type="number"
             placeholder="_"
             id={userid}
             value={point}
-            onChange={(e) => setPoint(e.target.value)}
+            onChange={InputChangeValue}
+            onKeyPress={handleKeyPress}
           />
-          <button className="Add-button" onClick={handleUpdate}>
-            save
-          </button>
         </>
       )}
     </SAdd>
